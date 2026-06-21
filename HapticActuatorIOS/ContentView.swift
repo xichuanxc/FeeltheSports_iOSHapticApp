2b43fd1db16618a653cd7f6da9daeb97bbfd6ded
XC <xichuanxc@gmail.com>
2026-06-22 09:35:28 +1200

    Initial Commit

---
+++ HapticActuatorIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HapticActuatorIOS
+//
+//  Created by CHUAN XI on 22/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HapticActuatorIOS/HapticActuatorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticActuatorIOSApp.swift
+//  HapticActuatorIOS
+//
+//  Created by CHUAN XI on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HapticActuatorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
